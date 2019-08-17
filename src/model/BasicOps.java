953b9a28e63904ee30f9a1540fcff273b81fce36
--replace-all <kamneklogs@gmail.com>
2019-08-16 20:29:27 -0500

    this got good

---
+++ src/model/BasicOps.java
@@ -0,0 +1,12 @@
+package model;
+import java.lang.Math;
+public class BasicOps {
+    public BasicOps(){
+     
+    }
+
+    public double sum(double num1, double num2){
+        return num1 +num2;
+    }
+
+}
--- src/ui/Calculator.java
+++ src/ui/Calculator.java
@@ -1,25 +1,40 @@
 package ui;
-
+import model.*;
 import java.util.Scanner;
 
 public class Calculator {
 
-    static int opt;
-    static Scanner r = new Scanner(System.in);
+  
+  static int opt, optMenu;
+  static Scanner r = new Scanner(System.in);
+  private BasicOps bOps;
 
-    public static void main(String[] args) {
-        System.out.println("**********************************************************************************************");
-        System.out.println("|                                   CALCULATOR VERSION 1.9                                    |");
-        System.out.println("**********************************************************************************************\n");
-    }
+  public Calculator() {
+    bOps = new BasicOps();
+  }
 
+  public static void main(String[] args) {
+    optMenu = o;
+    System.out
+        .println("**********************************************************************************************");
+    System.out
+        .println("|                                  CAMILO'S CALCULATOR VERSION 1.9                           |");
+    System.out
+        .println("**********************************************************************************************\n");
     do{
-        System.out.println("Opciones: \n1. Suma.");
-        opt = r.nextInt();
-        switch(opt){
-            case 1:
-                
-            break;
-        }
-     }
+      System.out.println(
+        "Opciones:\n1. Suma2. Resta.\n3. División.\n4. Multiplicación.\n5. Módulo.\n6. Seno.\n7. Coseno.\n8. Tangente.\n9. Logaritmo base 10 y base n.\n10. Raíz cuadrada.\n11. Raíz n-ésima.\n12. Potenciación.\n13. Base 10 de un número x.\n14. Memoria de los resultados de las últimas 10 operaciones. (Estos resultados pueden ser usados en la operación actual del usuario). \n15. El factorial de un número.\n16. La conversión de grados a radianes y viceversa.\n17. La conversión entre binarios, decimales y hexadecimales.");
+    opt = r.nextInt();
+    switch(opt){
+      case 1:
+        
+      break;
+    }
+    }while(optMenu == 0);
+  }
+
+  
+
+  
+
 }
