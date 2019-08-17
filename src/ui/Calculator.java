84dc821f914e54fd79687c72102a7da91220aff0
--replace-all <kamneklogs@gmail.com>
2019-08-16 19:48:43 -0500

    ui init

---
+++ src/ui/Calculator.java
@@ -0,0 +1,25 @@
+package ui;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    static int opt;
+    static Scanner r = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("**********************************************************************************************");
+        System.out.println("|                                   CALCULATOR VERSION 1.9                                    |");
+        System.out.println("**********************************************************************************************\n");
+    }
+
+    do{
+        System.out.println("Opciones: \n1. Suma.");
+        opt = r.nextInt();
+        switch(opt){
+            case 1:
+                
+            break;
+        }
+     }
+}
