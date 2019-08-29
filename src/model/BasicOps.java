6abfb2ebfc562117d015c977c1fa7f44499789a8
Cristhian Camilo Gutierrez Cordoba <1007379759@academico.icesi.edu.co>
2019-08-29 10:36:35 -0500

    new changes

--- src/model/BasicOps.java
+++ src/model/BasicOps.java
@@ -16,47 +16,34 @@ public class BasicOps {
      * <b>post:</b> The sum hs been printed <br>
      * @param num1 is a number . num1 != null
      * @param num2 is a number . num2 != null
-     * @throws Exception I don't know
      */
 
-    public void sum() {
-        System.out.println("Ingrese el primer numero: ");
-        num1 = r.nextDouble();
-        System.out.println("Ingrese el segundo numero: ");
-        num2 = r.nextDouble();
-        System.out.println("El resultado es: " + (num1 + num2));
+    public double sum(double num1, double num2) {
+        return num1 + num2;
     }
 
-    public void subt() {
-        System.out.println("Ingrese el primer numero: ");
-        num1 = r.nextDouble();
-        System.out.println("Ingrese el segundo numero: ");
-        num2 = r.nextDouble();
-        System.out.println("El resultado de la resta es: " + (num1 - num2));
+    public double subt(double num1, double num2) {
+        return num1 - num2;
     }
 
-    public void div() {
-        System.out.println("Ingrese el numerador: ");
-        num1 = r.nextDouble();
-        System.out.println("Ingrese el denominador: ");
-        num2 = r.nextDouble();
-        System.out.println("El resultado de la division es: " + (num1 / num2));
+    public double div(double num1, double num2)throws ArithmeticException {
+        try {
+            return num1 / num2;
+        } catch (ArithmeticException e) {
+            System.err.println("ERROR DEMONIACO\n  Detalles: " + e + " NO SE PUEDE DIVIDIR POR CERO");
+        }
     }
 
-    public void multi() {
-        System.out.println("Ingrese el primer factor: ");
-        num1 = r.nextDouble();
-        System.out.println("Ingrese el segundo factor: ");
-        num2 = r.nextDouble();
-        System.out.println("El resultado de la multipplicacion es: " + (num1 * num2));
+    public double multi(double num1, double num2) {
+        return num1 * num2;
     }
 
-    public void mod() {
-        System.out.println("Ingrese el numerador: ");
-        num1 = r.nextDouble();
-        System.out.println("Ingrese el denominador: ");
-        num2 = r.nextDouble();
-        System.out.println("El modulo es: " + (num1 % num2));
+    public double mod(double num1, num2)throws ArithmeticException {
+        try {
+            return num1 % num2;
+        } catch (ArithmeticException e) {
+            System.err.println("ERROR DEMONIACO\n  Detalles: " + e + " NO SE PUEDE DIVIDIR POR CERO");
+        };
     }
 
 }
